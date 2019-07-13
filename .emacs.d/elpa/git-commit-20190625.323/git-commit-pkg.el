2323ce39997f4884bd1472f9df2be743803cbb4b
renzmann <32076780+renzmann@users.noreply.github.com>
2019-07-13 16:21:09 -0500

    add .emacs.d

---
+++ .emacs.d/elpa/git-commit-20190625.323/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20190625.323" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "d1e1e3bfb35f1600913f58cfb96419420c77e120" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
---
+++ .emacs.d/transient/history.el
@@ -0,0 +1,8 @@
+((magit-branch nil)
+ (magit-commit nil)
+ (magit-dispatch nil)
+ (magit-fetch nil)
+ (magit-merge
+  ("--no-ff"))
+ (magit-pull nil)
+ (magit-push nil))
