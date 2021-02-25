e8785bd4e1247019ad3609439177557578e1006e
Robert Enzmann <robbenzmann@gmail.com>
2021-02-24 21:13:33 -0500

    Start work on cmd line options

--- install.sh
+++ install.sh
@@ -1,15 +1,47 @@
 #!/bin/bash
 
-rm -rf $HOME/.emacs.d
-mkdir $HOME/.emacs.d
+function usage {
+    echo "./$(basename $0)"
+    echo "  -h .......... Show help and exit"
+    echo "  -f .......... Rebuild installation directories before linking"
+}
 
-for filename in .emacs.d/* .bashrc .profile .vimrc .pylintrc
+
+optstring=":h:f"
+
+
+while getopts ${optstring} arg
+do
+    case ${arg} in
+        h)
+            usage
+            exit 0
+            ;;
+        f)
+            rm -rf $HOME/.emacs.d
+            mkdir $HOME/.emacs.d
+
+            rm -rf $HOME/.config/nvim
+            mkdir $HOME/.config/nvim
+            ;;
+        :)
+            echo "$0: Must supply an argument to -$OPTARG." >&2
+            exit 0
+            ;;
+        ?)
+            echo "invalid option: $arg"
+            exit 2
+            ;;
+    esac
+done
+
+
+for filename in .emacs.d/* .bashrc .profile .pylintrc
 do
     ln -nfs $(pwd)/$filename $HOME/$filename
 done
 
-rm -rf $HOME/.config/nvim
-mkdir $HOME/.config/nvim
+
 for filename in nvim/*
 do
     ln -nfs $(pwd)/$filename $HOME/.config/$filename
