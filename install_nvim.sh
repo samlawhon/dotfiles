532061d292c6308283500a24b329200b89048f98
Robb <robb@algorexhealth.com>
2021-06-15 14:28:01 +0000

    add git clones of config

--- install_nvim.sh
+++ install_nvim.sh
@@ -1,8 +1,13 @@
 cd ~
+
 curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 chmod u+x nvim.appimage
 echo "alias nvim='~/nvim.appimage'" >> ~/.bashrc
 
 sudo npm install -g bash-language-server
 sudo npm install -g pyright
+
+git clone --recursive git@github.com:renzmann/.vim ~/.vim
+git clone --recursive git@github.com:renzmann/config-nvim ~/.config/nvim
+
 cd -
