595a87b12daffbe7797cbcec0a56d29fd598282b
Robb <robb@algorexhealth.com>
2021-06-15 14:21:36 +0000

    nightly version of nvim

--- install_nvim.sh
+++ install_nvim.sh
@@ -1,5 +1,5 @@
 cd ~
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 chmod u+x nvim.appimage
 echo "alias nvim='~/nvim.appimage'" >> ~/.bashrc
 
