41f670ffba86db54115fb2a873d880ed6f168cf7
Robb <robb@algorexhealth.com>
2021-06-16 11:45:53 +0000

    add virtual env for nvim on install

--- install_nvim.sh
+++ install_nvim.sh
@@ -2,12 +2,24 @@ cd ~
 
 curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 chmod u+x nvim.appimage
-echo "alias nvim='~/nvim.appimage'" >> ~/.bashrc
+
+if [[ ! -z $(awk '$0 == "alias nvim=~/nvim.appimage"' ~/.bashrc) ]]; then
+    echo "alias nvim='~/nvim.appimage'" >> ~/.bashrc
+else
+    echo "alias already in ~/.bashrc"
+fi
 
 sudo npm install -g bash-language-server
 sudo npm install -g pyright
 
 git clone --recursive git@github.com:renzmann/.vim ~/.vim
 git clone --recursive git@github.com:renzmann/config-nvim ~/.config/nvim
+
+python3 -m venv .nvim.venv
+source ./.nvim.venv/bin/activate
+pip install black
+pip install neovim
+pip install flake8
+deactivate
 
 cd -
